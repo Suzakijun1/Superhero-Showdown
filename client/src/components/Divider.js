f9738e7f54ff1145d18c095974c80dedff795b6a
Suzakijun1 <dyong1997@gmail.com>
2025-10-25 20:53:48 -0400

    added divider.js

---
+++ client/src/components/Divider.js
@@ -0,0 +1,10 @@
+import React from "react";
+import line from "../images/hr-1.png";
+
+export default function Divider() {
+  return (
+    <div>
+      <img src={line} alt="divider" />
+    </div>
+  );
+}
