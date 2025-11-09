7201871e40d628a0fe37d24733d3b77557057ad8
Suzakijun1 <dyong1997@gmail.com>
2025-11-08 21:33:17 -0500

    added yourprofile for pages

---
+++ client/src/pages/YourProfile.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+import Profile from "../components/Profile";
+import Footer from "../components/Footer";
+import { useQuery } from "@apollo/client";
+import { USER_DATA } from "../utils/queries";
+import bgImage from "../images/battlegrounds-14.jpeg";
+import { faRefresh } from "@fortawesome/free-solid-svg-icons";
+
+export default function YourProfile() {
+  const [userData, setUserData] = useState({
+    me: {
+      username: "Loading",
+      higherLowerGamesPlayed: 0,
+      higherLowerGameHighestScore: 0,
+      draftGamesPlayed: 0,
+      draftGameWins: 0,
+      draftGameLosses: 0,
+    },
+  });
+
+  const { loading, data, refetch } = useQuery(USER_DATA);
+
+  useEffect(() => {
+    refetch();
+  }, []);
+
+  useEffect(() => {
+    setUserData((prevUserData) => {
+      return {
+        ...prevUserData,
+        ...data,
+      };
+    });
+  }, [data]);
+  return (
+    <div
+      className="bg-cover bg-center tracking-widest w-full"
+      style={{
+        backgroundImage: `url(${bgImage})`,
+      }}
+    >
+      <h1 className="text-3xl font-bold text-center pt-20 sh-background-1"></h1>
+      <Profile userData={userData} />
+    </div>
+  );
+}
