b16068320ef8aab2e3f81f3151142cec0123b1dd
Suzakijun1 <dyong1997@gmail.com>
2025-11-08 21:32:42 -0500

    added home.js for pages

---
+++ client/src/pages/Home.js
@@ -0,0 +1,27 @@
+import React, { useEffect } from "react";
+import GameModesComponent from "../components/GameMode";
+import Footer from "../components/Footer";
+import bgImage from "../images/battlegrounds-1.jpeg";
+import { useQuery } from "@apollo/client";
+import { USER_DATA } from "../utils/queries";
+
+export default function Home() {
+  const { loading, data } = useQuery(USER_DATA);
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
+  return (
+    <div
+      className="bg-cover bg-center"
+      style={{
+        backgroundImage: `url(${bgImage})`,
+      }}
+    >
+      <h1 className="heading-2 text-3xl font-bold text-center pt-20 sh-background-1 tracking-widest">
+        Welcome to the Game!
+      </h1>
+      <GameModesComponent />
+    </div>
+  );
+}
