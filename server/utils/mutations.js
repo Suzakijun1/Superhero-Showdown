30804f16028c35fb5384d017d39a524410b73f76
Suzakijun1 <dyong1997@gmail.com>
2025-11-16 16:44:45 -0500

    added mutations.js

---
+++ server/utils/mutations.js
@@ -0,0 +1,39 @@
+import { gql } from "@apollo/client";
+
+export const ADD_USER = gql`
+  mutation Mutation($email: String!, $username: String!, $password: String!) {
+    addUser(email: $email, username: $username, password: $password) {
+      user {
+        username
+        password
+        email
+      }
+      token
+    }
+  }
+`;
+export const LOGIN_USER = gql`
+  mutation Mutation($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      token
+    }
+  }
+`;
+export const UPDATE_HIGHSCORE = gql`
+  mutation Mutation($streak: Int!) {
+    updateHigherLowerHighestScore(streak: $streak) {
+      higherLowerGamesPlayed
+      higherLowerGameHighestScore
+    }
+  }
+`;
+
+export const UPDATE_DRAFT_GAME_STATS = gql`
+  mutation Mutation($won: Boolean!) {
+    updateDraftGameStats(won: $won) {
+      draftGamesPlayed
+      draftGameWins
+      draftGameLosses
+    }
+  }
+`;
