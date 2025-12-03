fdcefa534d3242b041ce0e35c0a2b80df0153e51
Suzakijun1 <dyong1997@gmail.com>
2025-12-02 19:34:28 -0500

    added queries.js for client

---
+++ client/src/utils/queries.js
@@ -0,0 +1,45 @@
+import { gql } from "@apollo/client";
+
+export const HERO_IMG = gql`
+  query Hero($heroId: String!) {
+    hero(id: $heroId) {
+      name
+      image {
+        url
+      }
+      powerstats {
+        strength
+        speed
+        power
+        durability
+        intelligence
+        combat
+      }
+      id
+    }
+  }
+`;
+
+export const USER_HIGHSCORE = gql`
+  query User {
+    user {
+      higherLowerGameHighestScore
+    }
+  }
+`;
+
+export const USER_DATA = gql`
+  query Query {
+    me {
+      username
+      password
+      higherLowerGamesPlayed
+      higherLowerGameHighestScore
+      draftGamesPlayed
+      draftGameWins
+      draftGameLosses
+      email
+      _id
+    }
+  }
+`;
